6e794747bee2ed3421b08e7dc8817f06f04d965e
benaryaau-art <benarya.au@gmail.com>
2026-08-11 22:46:11 +1000

    Bust global fixes cache for promo animation

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ export default {
     if (!type.includes('text/html')) return response;
 
     let html = await response.text();
-    const globalFixScript = '<script defer src="/assets/site-fixes.js?v=20260811a"></script>';
+    const globalFixScript = '<script defer src="/assets/site-fixes.js?v=20260811b"></script>';
     if (!html.includes('/assets/site-fixes.js')) {
       html = html.replace('</body>', `${globalFixScript}</body>`);
     }
