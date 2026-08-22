ad9a9a5ad8149eaf3ed51c3424a0267fd639efdc
benaryaau-art <benarya.au@gmail.com>
2026-08-22 20:21:59 +1000

    Refresh repaired Hotel Deals image

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ export default {
 
     const isHome = url.pathname === '/' || url.pathname === '/index.html';
     if (isHome && !html.includes('assets/site-fixes.js')) {
-      html = html.replace('</body>', '<script src="/assets/site-fixes.js?v=20260822-coast-iphone"></script></body>');
+      html = html.replace('</body>', '<script src="/assets/site-fixes.js?v=20260822-coast-safari-fixed"></script></body>');
     }
 
     const headers = new Headers(response.headers);
